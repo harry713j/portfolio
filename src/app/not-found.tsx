d9538906491bdfc68ce22c684e7d8b890ad08119
Harry <hariharasethi89@gmail.com>
2025-09-18 11:10:34 +0530

    Add Not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import { Container } from "@/components/ui/container";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <Container className="flex-row justify-center items-center py-0">
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-3xl font-fira-mono font-semibold text-slate-600 opacity-70 dark:text-slate-200 ">
+          404 <span className="font-thin">|</span> Not Found
+        </h2>
+        <p className="text-base font-inter text-slate-600 opacity-50 dark:text-slate-200 ">
+          The page you are looking for is not exists
+        </p>
+        <Link
+          href="/"
+          className="text-base font-inter text-blue-400 bg-blue-100 px-4 py-2 rounded-md transition-colors ease-in-out duration-200 hover:text-blue-600 hover:bg-blue-200 dark:bg-gray dark:text-slate-200 dark:hover:bg-gray-500 dark:hover:text-slate-100 "
+        >
+          Go to Home
+        </Link>
+      </div>
+    </Container>
+  );
+}
